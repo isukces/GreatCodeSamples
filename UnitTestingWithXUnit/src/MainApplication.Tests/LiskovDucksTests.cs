35c6997a796e2b268209a02a788b8a3f4f8b930e
Piotr Stęclik <piotr@steclik.com>
2017-07-15 12:07:30 +0200

    LiskovDucks

---
+++ UnitTestingWithXUnit/src/MainApplication.Tests/LiskovDucksTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace MainApplication.Tests
+{
+    public class LiskovDucksTests
+    {
+        [Fact]
+        public void T01_Duck_should_swimm()
+        {
+            CheckDuckContracts(new Duck());
+        }
+
+        /// <summary>
+        /// ElectricDuck break Liskov rule
+        /// </summary>
+        [Fact]        
+        public void T02_ElectricDuck_should_swimm()
+        {
+            CheckDuckContracts(new ElectricDuck());
+        }
+
+        private static void CheckDuckContracts(IDuck duck)
+        {
+            Assert.False(duck.IsSwimming);
+            duck.Swim();
+            Assert.True(duck.IsSwimming);
+        }
+    }
+}
---
+++ UnitTestingWithXUnit/src/MainApplication/LiskovDucks.cs
@@ -0,0 +1,35 @@
+﻿namespace MainApplication
+{
+    public interface IDuck
+    {
+        void Swim();
+        bool IsSwimming { get; }
+    }
+
+    public class Duck : IDuck
+    {
+        public void Swim()
+        {
+            IsSwimming = true;
+        }
+
+        public bool IsSwimming { get; set; }
+    }
+
+    public class ElectricDuck : IDuck
+    {
+        public void Swim()
+        {
+            if (_isTurnedOn)
+                IsSwimming = true;
+        }
+
+        public void TurnOn()
+        {
+            _isTurnedOn = true;
+        }
+
+        public bool IsSwimming { get; set; }
+        private bool _isTurnedOn;
+    }
+}
